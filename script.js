85fea688712720a6e3e4cb94f947d943e911c35b
Maria Kvist <mariakvist85@gmail.com>
2025-11-25 16:08:55 +0100

    made it more userfriendly

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let number = Math.floor(Math.random() * 100 + 1);
+function newGame(){
+    number = Math.floor(Math.random() * 100 + 1);
+    document.getElementById("guess").focus();
+document.getElementById("guess").select();
+
+}
 function checkGuess(){
     let guess = document.getElementById("guess").value;
     let msg = "";
@@ -8,7 +14,11 @@ function checkGuess(){
         msg = guess + " was too low. Try again";
     else {
         msg = guess + " is Correct!";
+        msg = msg + " Try a new game :";
+        newGame();
     }
 let output = document.getElementById("output");
 output.innerHTML = msg;
+document.getElementById("guess").focus();
+document.getElementById("guess").select();
 }
