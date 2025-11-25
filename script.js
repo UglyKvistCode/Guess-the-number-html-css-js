7cfb3b6e4e9fb8d5ce5b88b6a54a1ecdaa33ec12
Maria Kvist <mariakvist85@gmail.com>
2025-11-25 16:30:19 +0100

    added how many tries

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let number = Math.floor(Math.random() * 100 + 1);
+let tries = 0;
 function newGame(){
     number = Math.floor(Math.random() * 100 + 1);
     document.getElementById("guess").focus();
 document.getElementById("guess").select();
+tries = 0;
 
 }
 function checkGuess(){
     let guess = document.getElementById("guess").value;
     let msg = "";
+    tries++;
     if (guess > number)
         msg = guess + " was too high. Try again.";
     else if (guess < number)
@@ -15,6 +18,7 @@ function checkGuess(){
     else {
         msg = guess + " is Correct!";
         msg = msg + " Try a new game :";
+        msg = msg + " You won in " + tries + " tries.";
         newGame();
     }
 let output = document.getElementById("output");
